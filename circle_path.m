a6b9d5da13ae884eab464ad3eccc11ba6378c7bf
ldfleury <ld.fleury@gmail.com>
2016-04-23 19:14:38 -0400

    Update circle_path.m

--- circle_path.m
+++ circle_path.m
@@ -9,7 +9,7 @@ Z = h*ones(1,n);
 X = R*sin(t);
 Y = R*cos(t);
 for i = 1: length(t_travel)
-x(:,i) = x0 + t_travel(i)/travel.*([X(1);Y(1);Z(1)]-x0);
+x(:,i) = x0(1:3) + t_travel(i)/travel.*([X(1);Y(1);Z(1)]-x0(1:3));
 
 end
 x;
