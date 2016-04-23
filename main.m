58d85b7c68dbb3e9d272ec70a6664b44fac2f7dc
Bereket Abraham <babraham42@gmail.com>
2016-04-23 19:18:04 -0400

    rename

---
+++ main.m
@@ -0,0 +1,38 @@
+% Kinematics, Dynamics, and Control (16-711)
+% Bereket Abraham, Laura Fleury
+% Final Project
+% Rocket Powered Descent Simulation
+
+% load constant parameters
+consts = get_consts();
+
+% x = x,y,z, vx,vy,vz, theta,phi,psi, wx,wy,wz
+x0 = [10,10,100, 0,0,0, 0,0,0, 0,0,0]';
+tspan = linspace(consts.trange(1), consts.trange(2), consts.nIter);
+
+% generate trajectory of desired positions, xd = [x y z psi]
+trajectory = guidance(x0, 'hover');
+
+% interpolate and differentiate
+xd = interp1(trajectory(1,:), trajectory(2:end,:), tspan, 'cubic');
+vd = diff(xd)./diff(td);
+ad = diff(vd)./diff(td(1:end-1));
+vd(:,end+1) = [0 0 0 0]';
+ad(:,end+1) = [0 0 0 0]'; ad(:,end+1) = [0 0 0 0]';
+trajectory = [trajectory; vd; ad];
+
+% calculate gains for PD control, [Kp; Kd]
+K = calculateGains(consts);
+
+% Integrate system
+[X, Xd, U] = simulate(tspan, x0, trajectory, consts, K);
+
+% J = compute_score(T, X, trajectory, consts);
+% disp(['Score: ' num2str(J)]) ;
+
+% Plots
+plotResults(T, X, Xd, U);
+
+% Animation
+animate3(T, X, Xd, U);
+
