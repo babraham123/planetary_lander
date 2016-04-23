0014548d02d662d72b92615984a6984bfa78e5b5
ldfleury <ld.fleury@gmail.com>
2016-04-23 17:51:29 -0400

    Update circle_path.m

--- circle_path.m
+++ circle_path.m
@@ -3,13 +3,13 @@ function traj = circle_path(R,h,x0)
 dt = 0.1;
 travel = 8;
 t_travel = 0:dt:travel;
-t = travel+dt:dt:100;
+t = travel+dt:dt:20;
 n = length(t);
 Z = h*ones(1,n);
 X = R*sin(t);
 Y = R*cos(t);
 for i = 1: length(t_travel)
-x(:,i) = x0 - t_travel(i)/travel.*[X(1);Y(1);Z(1)];
+x(:,i) = -x0 + t_travel(i)/travel.*[X(1);Y(1);Z(1)];
 
 end
 x;
