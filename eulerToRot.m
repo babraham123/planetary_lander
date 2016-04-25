dce3994b2de0c86dd352daaaa3e7edab159d5031
ldfleury <ld.fleury@gmail.com>
2016-04-24 20:48:07 -0400

    Added files via upload

--- eulerToRot.m
+++ eulerToRot.m
@@ -8,8 +8,8 @@ s2 = sin(eulerAngles(2));
 c3 = cos(eulerAngles(3));
 s3 = cos(eulerAngles(3));
 
-R = [c3*c1-s2*s3*s1 -c2*c3 c3*s1+c1*s2*s3;
-c1*s3+c3*s2*s1 c2*c3 s3*s1-c3*c1*s2;
--c2*s1 s2 c2*c2];
+R = [c3*c2-s1*s3*s2 -c1*c3 c3*s2+c2*s1*s3;
+c2*s3+c3*s1*s2 c1*c3 s3*s2-c3*c2*s1;
+-c1*s2 s1 c1*c2];
 
 end
